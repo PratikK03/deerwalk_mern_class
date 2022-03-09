b46803b96973314f6a129bbf80d3761c865d37de
Pratik Kunwar <kunwarpratik22@gmail.com>
2022-03-09 17:30:41 +0545

    implementing react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from 'react-bootstrap';
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
     return (
-        <>
-            <h1>Welcome to Deerwalk E-commerce site</h1>
-            <Header></Header>
+        
+            <Router>
+                <Header></Header>
 
-            <main>
-                <Container>
-                    <HomeScreen />
-                </Container>
-            </main>
+                <main>
+                    <Container>
+                        <h1>Welcome to Deerwalk E-commerce site</h1>
+                        <Routes>
+                        <Route path="/" element= {<HomeScreen />} exact />
+                        <Route path="/product/:id" element={<ProductScreen />} />
+                        </Routes>
+                       
+                    </Container>
+                </main>
 
-            <Footer></Footer>
-        </>
+                <Footer></Footer>
+            </Router>
+        
     );
 };
 
